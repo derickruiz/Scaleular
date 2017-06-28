ee58a590b6bc627f15f4b99f84eb801c6c5c3c86
Derick Ruiz <derick.r.ruiz@gmail.com>
2017-06-28 22:07:40 +0700

    Updating to refer to the data in data.js. Makes the file smaller, Nice.

--- scaleular.js
+++ scaleular.js
@@ -1,209 +1,11 @@
-const DATA = {
-  baseFontSize: 16,
-  scale: 1.272,
-  numberOfResizePoints: 6,
-  numberOfSizes: 8
-};
-
-const VARIABLES = `
-
-  $fontFamily-text:   Arial, sans-serif;
-  $fontFamily-boldText:   Arial, sans-serif;
-  $fontFamily-italicText: Arial, sans-serif;
-  $fontFamily-condensedText: Arial, sans-serif;
-
-`;
-
-const LINE_HEIGHTS = ["1", "1.2", "1.25", "1.3", "1.5", "1.618", "2"];
-const COLORS = ["#4C4C4C", "#42ACEE", "#0007DC"];
-
-const PURE_LAYOUT = [
-  {
-    className: "TopSpacer",
-    properties: ["padding-top"]
-  },
-  {
-    className: "BottomSpacer",
-    properties: ["padding-bottom"]
-  },
-  {
-    className: "RightSpacer",
-    properties: ["margin-left"]
-  },
-  {
-    className: "LeftSpacer",
-    properties: ["margin-left"]
-  },
-  {
-    className: "HorizontalConstrainer",
-    properties: ["padding-left", "padding-right"]
-  },
-  {
-    className: "VerticalConstrainer",
-    properties: ["padding-top", "padding-bottom"]
-  }
-];
-
-const NONPURE_LAYOUT = [
-  {
-    className: "TextAligner",
-    modifiers: [
-      {
-        modifierName:     "center",
-        modifierProperty: "text-align",
-        modifierValue:    "center"
-      },
-      {
-        modifierName:     "right",
-        modifierProperty: "text-align",
-        modifierValue:    "right"
-      },
-      {
-        modifierName:     "left",
-        modifierProperty: "text-align",
-        modifierValue:    "left"
-      }
-    ]
-  },
-  {
-    className: "HorizontalSizer",
-    modifiers: [
-      {
-        modifierName:     "max",
-        modifierProperty: "width",
-        modifierValue:    "100%"
-      },
-      {
-        modifierName:     "center",
-        modifierProperty: "margin",
-        modifierValue:    "0 auto"
-      }
-    ],
-    scaleProperty: ["max-width"]
-  },
-  {
-    className: "Shower",
-    modifiers: [
-      {
-        modifierName:     "hide",
-        modifierProperty: "display",
-        modifierValue:    "none"
-      },
-      {
-        modifierName:     "show",
-        modifierProperty: "display",
-        modifierValue:    "block"
-      }
-    ]
-  },
-  {
-    className: "Text",
-    defaultProperties: {
-      "font-family": "$fontFamily-text",
-      "font-weight": "normal",
-      "-webkit-font-smoothing": "antialiased",
-      "-moz-osx-font-smoothing": "grayscale"
-    },
-    scaleProperties: ["font-size"]
-
-  },
-  {
-    className: "Bold",
-    defaultProperties: {
-      "font-family": "$fontFamily-boldText",
-      "font-weight": "bold",
-      "-webkit-font-smoothing": "antialiased",
-      "-moz-osx-font-smoothing": "grayscale"
-    },
-    scaleProperties: ["font-size"],
-    colors: true,
-    lineHeights: true
-  },
-  {
-    className: "Italic",
-    defaultProperties: {
-      "font-family": "$fontFamily-italicText",
-      "font-weight": "normal",
-      "font-style":  "italic",
-      "-webkit-font-smoothing": "antialiased",
-      "-moz-osx-font-smoothing": "grayscale"
-    },
-    scaleProperties: ["font-size"],
-    color: true,
-    lineHeights: true
-  },
-  {
-    className: "Condensed",
-    defaultProperties: {
-      "font-family": "$fontFamily-condensedText",
-      "font-weight": "normal",
-      "-webkit-font-smoothing": "antialiased",
-      "-moz-osx-font-smoothing": "grayscale"
-    },
-    scaleProperties: ["font-size"],
-    color: true,
-    lineHeights: true
-  },
-  {
-    className: "Grid",
-    defaultProperties: {
-      "display": "flex",
-      "flex-wrap": "wrap",
-      "list-style": "none",
-      "margin": "0",
-      "padding": "0"
-    },
-    modifiers: [
-      {
-        modifierName:     "center",
-        modifierProperty: "justify-content",
-        modifierValue:    "center"
-      },
-      {
-        modifierName:     "inline",
-        modifierProperty: "display",
-        modifierValue:    "inline-flex"
-      },
-      {
-        modifierName:     "reverse",
-        modifierProperty: "flex-direction",
-        modifierValue:    "row-reverse"
-      }
-    ]
-  }
-];
-
-/* The typography modifier adds all of the typography related modifiers to each class.
- * --sublte, --tight, --light for example and their associated responsive styles
- *
- * The grid modifier adds all of the grid related modifiers to the grid.
- * --center, --inline, --reverse
- */
-
-
-// 1. Compile template function
-// var tempFn = doT.template(`
-// {{?it.prefix}}
-// .{{=it.prefix}}-{{=it.className}} {
-// {{??}}
-// .{{=it.className}} {
-// {{?}}
-//  {{=it.property}}: {{=it.value}}
-// }`);
-//
-// // 2. Use template function as many times as you like
-// const resultText = tempFn({
-//   prefix: "four",
-//   className: "Hello",
-//   property: "display",
-//   value: "block"
-// });
+const DATA = require("data");
 
 const doT = require('dot');
 const numberConverter = require('number-to-words');
 // const colorNamer = require('color-namer');
 
 // STEP 1. Generate scale variables
+
 /* Gets the next level down for the scale given the base font size. */
 const getDownRemNumber = function (baseFontSize, scale, number) {
 
@@ -361,10 +163,10 @@ const generatePureLayout = function () {
     let pureLayoutText = "";
 
     // Need to get the appropriate $scale variable in here and pass that in when creating the
-    for (let i = 0; i < PURE_LAYOUT.length; i += 1) {
+    for (let i = 0; i < DATA.PURE_LAYOUT.length; i += 1) {
       pureLayoutText += allLayoutParts({
-        className: PURE_LAYOUT[i].className,
-        generatedClasses: generateScaledClasses(PURE_LAYOUT[i].className, PURE_LAYOUT[i].properties)
+        className: DATA.PURE_LAYOUT[i].className,
+        generatedClasses: generateScaledClasses(DATA.PURE_LAYOUT[i].className, DATA.PURE_LAYOUT[i].properties)
       });
     }
 
@@ -374,19 +176,19 @@ const generatePureLayout = function () {
 const generateNonPureLayout = function () {
 
   // Need to get the appropriate $scale variable in here and pass that in when creating the
-  for (let i = 0; i < NONPURE_LAYOUT.length; i += 1) {
+  for (let i = 0; i < DATA.NONPURE_LAYOUT.length; i += 1) {
 
 
-    if ("modifiers" in NONPURE_LAYOUT[i]) {
+    if ("modifiers" in DATA.NONPURE_LAYOUT[i]) {
 
       let modifierClasses = [];
 
-      NONPURE_LAYOUT[i].modifiers.forEach(function (modifier) {
+      DATA.NONPURE_LAYOUT[i].modifiers.forEach(function (modifier) {
 
         const arrayedModifierProperties = [modifier.modifierProperty];
 
         const modifierClass = singleLayoutClass({
-          className: NONPURE_LAYOUT[i].className,
+          className: DATA.NONPURE_LAYOUT[i].className,
           modifier: modifier.modifierName,
           properties: arrayedModifierProperties,
           propertyValue: modifier.modifierValue
@@ -397,8 +199,8 @@ const generateNonPureLayout = function () {
 
     }
 
-    if ("scaleProperties" in NONPURE_LAYOUT[i]) {
-      const scaledClasses = generateScaledClasses(NONPURE_LAYOUT[i].className, NON_PURELAYOUT[i].scaleProperties);
+    if ("scaleProperties" in DATA.NONPURE_LAYOUT[i]) {
+      const scaledClasses = generateScaledClasses(DATA.NONPURE_LAYOUT[i].className, DATA.NONPURE_LAYOUT[i].scaleProperties);
     }
 
   }
