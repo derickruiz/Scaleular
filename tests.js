58acb89b27bad2f990fedea20e5777a7fe2a60b8
Derick Ruiz <derick.r.ruiz@gmail.com>
2017-06-30 01:34:14 +0700

    Removing the useless callback and the console logs.

--- tests.js
+++ tests.js
@@ -4,8 +4,4 @@ const testrunner = require("qunit");
 testrunner.run({
     code: "./utils.js",
     tests: "./utils.tests.js"
-}, function (one, two, three) {
-  console.log("one", one);
-  console.log("two", two);
-  console.log("three", three);
 });
